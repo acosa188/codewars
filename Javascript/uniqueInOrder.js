5711e29bd4b634e8f64b9f5ffffbe911d3f9faf4
Arjun Cosare <arjun.cosare@outlook.com>
2021-02-03 13:38:36 -0700

    Solution

---
+++ Javascript/uniqueInOrder.js
@@ -0,0 +1,40 @@
+/**
+ * Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+ */
+
+var uniqueInOrder = function (iterable) {
+    //your code here - remember iterable can be a string or an array
+    let holder = ""
+    let newArr = []
+
+    // String
+    if (typeof iterable === 'string') {
+        iterable.split('').map(x => {
+            if (x !== holder) {
+                newArr.push(x)
+                holder = x
+            }
+        })
+    }
+    // Array
+    else {
+        iterable.map(x => {
+            if (x !== holder) {
+                newArr.push(x)
+                holder = x
+            }
+
+        })
+    }
+
+
+    return newArr
+}
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB'))

538fc573f348716a4e47e6fde9dc8525158ea322
Arjun Cosare <arjun.cosare@outlook.com>
2021-02-09 13:27:14 -0700

    solution 02/09/21

---
+++ Javascript/generateHashtag.js
@@ -0,0 +1,20 @@
+/**
+ * The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+Examples
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+ * @param {string} str 
+ */
+function generateHashtag (str) {
+    let cleanStr = str.trim().replace(/\s\s+/g,' ');
+    return (cleanStr != "") && (cleanStr.split(' ').join('').length + 1 <= 140) ? '#' + cleanStr.split(' ').map(x =>{return x[0].toUpperCase() + x.slice(1,x.length)}).join('') : false;
+}
